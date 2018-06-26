e6b20000c32de63d4ed2f228438e58bd8b51489d
WarWarMyint <warwarmyint9515@gmail.com>
2018-06-26 01:32:48 -0600

    ex101.py

--- ex101.py
+++ ex101.py
@@ -1,13 +1,13 @@
 "I am 6'2\" tall." # escape double-quote inside string
 'I am 6\'2" tall.' # escape single-quote inside string
-tabby_dog = "\tI'm tabbed in."
-persian_dog = "I'm split\non a line."
-backslash_dog = "I'm \\ a \\ car."
-fat_dog = """I'll do a list:
-    \t* Dog food
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ car."
+fat_cat = """I'll do a list:
+    \t* Cat food
     \t* Fishies
     \t* Catnip\n\t* Grass """
-print(tabby_dog)
-print(persian_dog)
-print(backslash_dog)
-print(fat_dog)
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
